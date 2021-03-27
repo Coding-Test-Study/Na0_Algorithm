2fe91ffd2e5f2eff8988faca13e49b699f80fd7c
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-03-27 22:09:34 +0900

    왕실의 나이트

--- Impelmentation/3.py
+++ Impelmentation/3.py
@@ -1,4 +1,5 @@
 n = input()
+
 dx = [2,2,-2,-2,1,-1,1,-1]
 dy = [1,-1,1,-1,2,2,-2,-2]
 
@@ -8,5 +9,5 @@ for i in range(8):
     if ord('a')<=ord(n[0])+dx[i]<=ord('h'):
             if 1<=int(n[1])+dy[i]<=8:
                 count += 1
-    
+
 print(count)
