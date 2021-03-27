e6954d8140d3e12e3208d80e36ed6eae5828332e
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-03-27 17:06:22 +0900

    큰 수의 법칙

---
+++ Greedy/1a.py
@@ -0,0 +1,27 @@
+# n, m, k를 공백으로 구분하여 입력받기
+n,m,k = map(int, input().split())
+# 더한 값을 저장할 변수
+count = 0
+
+# n개의 수를 공백으로 구분하여 리스트에 입력받기
+li = []
+li = list(map(int, input().split()))
+
+# 가장 큰수
+max_1 = max(li)
+# 가장 큰 수 리스트에서 제거
+li.remove(max_1)
+# 그 다음 큰 수 
+max_2 = max(li)
+
+while m > 0:
+    for i in range(0,k):
+        count += max_1
+        m -= 1
+        print(count, m)
+        if m<=0:
+            break
+    count += max_2
+    m -= 1
+
+print(count)
