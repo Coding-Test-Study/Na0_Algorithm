bb3cb490a0e660e0a98538499dfe9a1630e78324
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-05-14 22:02:57 +0900

    *다시보기*

--- Dynamic Programming/Q4. 효율적인 화폐 구성.py
+++ Dynamic Programming/Q4. 효율적인 화폐 구성.py
@@ -1,9 +1,19 @@
 n, m = map(int, input().split())
 v = []
 
-d = [0]*m
-
 for i in range(n):
     v.append(int(input()))
 
-for i in range(m):
+
+d = [10001]*(m+1)
+d[0] = 0
+
+for i in range(n): # 모든 화폐 가치에 대해
+    for j in range(v[i],m+1):
+        if d[j-v[i]] != 10001: # (i-k)원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j],d[j-v[i]]+1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
