0afa733291579980c266ad4b99e999e396db370d
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2022-01-11 15:58:49 +0900

    Update 3.py

--- Impelmentation/3.py
+++ Impelmentation/3.py
@@ -11,3 +11,19 @@ for i in range(8):
                 count += 1
 
 print(count)
+
+"""
+n = input()
+x, y = ord('a') -ord(n[0]) + 1, int(n[1])
+cnt = 0
+
+d = [(-2, 1), (-2, -1), (2, -1), (2, 1), (-1, 2), (-1, -2), (1, 2),(1, -2)]
+
+for i in range(8):
+    nx, ny = x + d[i][0], y + d[i][1]
+    
+    if 1<=nx<=8 and 1<=ny<=8:
+        cnt+=1
+        
+print(cnt)
+"""
